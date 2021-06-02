5a1c07187123ef0a790d5432372c967adf3be159
Nikki Mallory <nikkimallory13@gmail.com>
2021-06-02 00:08:06 -0500

    magic 8ball added

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 //variables
+const { response } = require("express");
 const express = require("express");
 const app = express();
 const port = 3000;
+// magic 8 ball variables
 
 
 // routes 
@@ -14,6 +16,33 @@ app.get(`/greeting/:name`, (req, res) => {
 app.get(`/tip/:total/:tipPercentage`, (req, res)=>{
     res.send("Your tip is" + `${req.params.tipPercentage/100}*${req.params.total}`);
 });
+
+// ##magic 8 ball
+let responses =["It is certain", 
+"It is decidedly so", 
+"Without a doubt", 
+"Yes definitely",
+"You may rely on it", 
+"As I see it yes", 
+"Most likely", 
+"Outlook good","Yes", 
+"Signs point to yes", 
+"Reply hazy try again", 
+"Ask again later",
+"Better not tell you now", 
+"Cannot predict now", 
+"Concentrate and ask again",
+"Don't count on it", 
+"My reply is no", 
+"My sources say no",
+"Outlook not so good", 
+"Very doubtful"];
+let random=Math.flow(Math.random() * response.length)
+app.get(`/magic/:question`, (req, res)=>{
+    res.send(
+
+    )
+})
 
 // listener
 app.listen(port, () => {
